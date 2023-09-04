997d18f31446470b7fe991aadcf548bb63c8d6c7
Elnur Mammadli <elnoor@users.noreply.github.com>
2023-09-04 09:12:17 +0000

    some cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,12 @@
-import Link from 'next/link'
-import { Suspense } from 'react'
 import Table from '@/components/table'
-import TablePlaceholder from '@/components/table-placeholder'
-import ExpandingArrow from '@/components/expanding-arrow'
 
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
-      <Link
-        href="https://vercel.com/templates/next.js/postgres-starter"
-        className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
-      >
-        <p>Deploy your own to Vercel</p>
-        <ExpandingArrow />
-      </Link>
       <h4 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center font-medium tracking-tight text-transparent md:text-4xl">
         Soccer Tracker
       </h4>
-      <Suspense fallback={<TablePlaceholder />}>
-        <Table />
-      </Suspense>
+      <Table />
     </main>
   )
 }
--- components/table.tsx
+++ components/table.tsx
@@ -1,6 +1,5 @@
 import { sql } from '@vercel/postgres'
 import { timeAgo } from '@/lib/utils'
-import RefreshButton from './refresh-button'
 import { seed } from '@/lib/seed'
 
 export default async function Table() {
@@ -17,20 +16,18 @@ export default async function Table() {
     } else { throw e }
   }
 
-  const { rows: players } = data
+  const players = data.rows
   const duration = Date.now() - startTime
-  console.log(data);
+
   return (
     <div className="w-full px-2">
+      <div className="flex justify-between max-w-xl mx-auto py-2">
+        <p className="space-y-1 text-sm text-gray-500">
+          Fetched {players.length} players in {duration}ms
+        </p>
+        <button className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-5 py-1 hover:shadow-lg active:shadow-sm transition-all">Admin</button>
+      </div>
       <div className="bg-white/30 p-4 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto">
-        <div className="flex justify-between items-center mb-4">
-          <div className="space-y-1">
-            <p className="text-sm text-gray-500">
-              Fetched {players.length} players in {duration}ms
-            </p>
-          </div>
-          <RefreshButton />
-        </div>
         <div className="divide-y divide-gray-900/5">
           {players.map((player) => (
             <div

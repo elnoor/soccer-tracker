5b8a42277dddf5d3aab004a41429a371a1346a5e
Elnur Mammadli <elnoor@users.noreply.github.com>
2023-09-06 10:32:27 +0000

    player profile form

--- components/playerProfile.jsx
+++ components/playerProfile.jsx
@@ -9,10 +9,7 @@ export default function PlayerProfile({ player }) {
   const [disabled, setDisabled] = useState(true);
 
   return (
-    <form
-      disabled={disabled}
-      className="flex flex-col md:flex-row md:items-center text-center gap-2 md:gap-0 w-full md:w-auto px-3"
-    >
+    <form className="flex flex-col md:flex-row md:items-center text-center gap-2 md:gap-0 w-full md:w-auto px-3">
       <p className="text-sm text-gray-500">
         Created{" "}
         <span
@@ -28,15 +25,22 @@ export default function PlayerProfile({ player }) {
         placeholder="Name"
         required={true}
         value={player.name}
-        className="bg-white text-gray-700 border focus:border-teal-500 rounded-full px-4 py-2 sm:mx-2 focus:outline-none focus:ring focus:ring-teal-200 focus:ring-opacity-40"
+        disabled={disabled}
       />
       <Input
         name="phone"
         placeholder="Phone"
         required={true}
         value={player.phone}
+        disabled={disabled}
       />
-      <Input name="email" placeholder="Email" required={true} value={"sss"} />
+      <Input
+        name="email"
+        placeholder="Email"
+        required={true}
+        value={player.email}
+        disabled={disabled}
+      />
       {disabled ? (
         <Button secondary onClick={() => setDisabled(false)}>
           Edit
@@ -46,16 +50,11 @@ export default function PlayerProfile({ player }) {
           <Button
             secondary
             className="w-full"
-            onClick={() => setDisabled(false)}
+            onClick={() => setDisabled(true)}
           >
             Cancel
           </Button>
-          <Button
-            className="md:order-first w-full"
-            onClick={() => setDisabled(false)}
-          >
-            Save
-          </Button>
+          <Button className="md:order-first w-full">Save</Button>
         </div>
       )}
     </form>

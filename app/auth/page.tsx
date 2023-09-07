296bc2d5d3e018a3042e996581e9df7f8bbcc9d6
Elnur Mammadli <elnoor@users.noreply.github.com>
2023-09-07 08:10:57 +0000

    minor design change

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,7 +2,7 @@ import { notFound, redirect } from "next/navigation";
 import Button from "@/components/button.jsx";
 import { isAdmin, checkPasswordAndSave } from "./actions";
 
-export default async function Auth() {
+export default async function Login() {
   if (await isAdmin()) {
     notFound();
   }
@@ -15,13 +15,13 @@ export default async function Auth() {
   }
 
   return (
-    <form action={submitPassword} className="flex">
+    <form action={submitPassword} className="flex gap-2">
       <input
         type="password"
         name="password"
         placeholder="Password"
         required={true}
-        className="bg-white text-gray-700 border focus:border-teal-500 rounded-full px-4 py-2 sm:mx-2 focus:outline-none focus:ring focus:ring-teal-200 focus:ring-opacity-40"
+        className="bg-white text-gray-700 border focus:border-teal-500 rounded-full px-4 py-2 focus:outline-none focus:ring focus:ring-teal-200 focus:ring-opacity-40"
       />
       <Button type="submit" onClick={undefined}>
         Enter

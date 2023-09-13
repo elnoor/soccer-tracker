589b55879504f18ff4d0177c1c69e39bd97ecffc
Elnur Mammadli <elnoor@users.noreply.github.com>
2023-09-13 10:11:27 +0000

    header footer changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,15 +20,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} min-h-screen`}>
-        <Link href="/">
-          <h4 className="pt-2 pb-4 bg-gradient-to-br from-black via-[#3E3E3E] to-[#7D7D7D] bg-clip-text text-center font-medium tracking-tight text-transparent text-3xl">
+        <header className="pt-2 pb-4 text-center">
+          <Link
+            href="/"
+            className="bg-gradient-to-br from-black via-[#3E3E3E] to-[#7D7D7D] bg-clip-text font-medium tracking-tight text-transparent text-3xl"
+          >
             Soccer Tracker
-          </h4>
-        </Link>
+          </Link>
+        </header>
         <main className="mx-auto p-2 flex flex-col items-center justify-center">
           {children}
         </main>
-        <footer className="text-sm text-center pt-2 pb-3 text-gray-500">
+        <footer className="sticky top-[100vh] text-sm text-center pt-2 pb-3 text-gray-500">
           elnoor was{" "}
           <Link
             href="https://github.com/elnoor/soccer-tracker"
